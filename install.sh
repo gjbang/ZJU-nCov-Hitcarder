d666d89d5cc4026d887ad90c5dce49a902285219
gjbang <793881807@qq.com>
2020-12-07 17:19:30 +0800

    [ADD] configure script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # pkg install git python3 tsh
 # git clone https://github.com/Tishacy/ZJU-nCov-Hitcarder.git
 apt-get install python
-pip3 install -r ./requirement.txt
+pip3 install -r ./requirements.txt
 echo "{
     "username": "$1",
     "password": "$2",
